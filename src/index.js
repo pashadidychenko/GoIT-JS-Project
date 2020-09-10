5aa7aea6a05bc8be81bf66be003509b5dc767fe0
Pasha.Didychenko <pasha.didychenko@gmail.com>
2020-09-10 19:15:30 +0300

    Great Project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/styles.scss';
+import './js/1initialHomePage';
+import './js/2searchAndPlaginationHomePage';
+import './js/3navigation';
+import './js/4filmDetailsPage';
+import './js/5libraryPage';
